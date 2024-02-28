f73d9aafc691ac7712968fad0732c4b8495b7610
Joe Runde <Joseph.Runde@ibm.com>
2024-02-28 16:15:03 -0700

    :construction_worker: Push images to quay

--- scripts/get_image_tags.sh
+++ scripts/get_image_tags.sh
@@ -2,8 +2,15 @@
 
 # Returns a space separated list of container image tags to be pushed to the
 # registry
+# If a repository is supplied as the first arg, (e.g. quay.io/foo/bar") then the tags will be fully qualified
 
 tags_to_push=""
+
+if [[ -n $1 ]]; then
+  repo_bit="${1}:"
+else
+  repo_bit=""
+fi
 
 # if running locally, i.e. CI is unset
 if [[ -z "${CI+x}" ]];
@@ -11,17 +18,20 @@ then
   commit="$(git rev-parse --short HEAD)"
   branch="$(git rev-parse --abbrev-ref HEAD)"
 
-  tags_to_push+="${commit}"
-  tags_to_push+=" ${branch}"
-  tags_to_push+=" ${branch}.${commit}"
+  tags_to_push+="${repo_bit}${commit}"
+  tags_to_push+=" ${repo_bit}${branch}"
+  tags_to_push+=" ${repo_bit}${branch}.${commit}"
 else
   # In CI, pull info from github env vars
   commit="${GITHUB_SHA:0:7}"
   build_ref="${GITHUB_REF_NAME}"
 
-  tags_to_push+="${commit}"
-  tags_to_push+=" ${build_ref}"
-  tags_to_push+=" ${build_ref}.${commit}"
+  tags_to_push+="${repo_bit}${commit}"
+  if [[ ! ${build_ref} =~ "merge" ]];
+  then
+    tags_to_push+=" ${repo_bit}${build_ref}"
+    tags_to_push+=" ${repo_bit}${build_ref}.${commit}"
+  fi
 fi
 
 echo "${tags_to_push}"
